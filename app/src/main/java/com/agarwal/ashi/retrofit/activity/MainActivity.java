ad191fa2e3f18dc59d3775688963394d929657ca
Ashi <500052000@stu.upes.ac.in>
2018-02-05 19:02:14 +0530

    Initial commit

---
+++ app/src/main/java/com/agarwal/ashi/retrofit/activity/MainActivity.java
@@ -0,0 +1,63 @@
+package com.agarwal.ashi.retrofit.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.agarwal.ashi.retrofit.R;
+import com.agarwal.ashi.retrofit.adapter.MoviesAdapter;
+import com.agarwal.ashi.retrofit.model.Movie;
+import com.agarwal.ashi.retrofit.model.MoviesResponse;
+import com.agarwal.ashi.retrofit.rest.ApiClient;
+import com.agarwal.ashi.retrofit.rest.ApiInterface;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    ProgressBar progressBar;
+
+    // TODO - insert your themoviedb.org API KEY here
+    private final static String API_KEY = "28a11636bd5628778806b29a1dc2a84b";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (API_KEY.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please obtain your API KEY from themoviedb.org first!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        progressBar=findViewById(R.id.progress);
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.movies_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ApiInterface apiService =
+                ApiClient.getClient().create(ApiInterface.class);
+
+        Call<MoviesResponse> call = apiService.getTopRatedMovies(API_KEY);
+        call.enqueue(new Callback<MoviesResponse>() {
+            @Override
+            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
+                int statusCode = response.code();
+                progressBar.setVisibility(View.INVISIBLE);
+                List<Movie> movies = response.body().getResults();
+                recyclerView.setAdapter(new MoviesAdapter(movies, R.layout.list_item_movie, getApplicationContext()));
+            }
+
+            @Override
+            public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                // Log error here since request failed
+                Log.e(TAG, t.toString());
+            }
+        });
+    }
+}
